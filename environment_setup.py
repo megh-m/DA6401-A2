77a2edb60eebebda0b395114ca24314e4d53aad0
megh-m <125548504+megh-m@users.noreply.github.com>
2025-04-20 01:15:16 +0900

    Create environment_setup.py

---
+++ environment_setup.py
@@ -0,0 +1,4 @@
+import os
+os.environ["WANDB_SILENT"] = "false"
+os.environ["WANDB_START_METHOD"] = "thread"
+os.environ["WANDB_API_KEY"] = "eb9574fa5b11da36782604ea27df8bf1989ddefd"  # Get from https://wandb.ai/authorize
